bf7cd3554fa17a51bd8aeb3d043ee0c441ec852c
Josh Vargas N77~ <joshvargasm3@hotmail.com>
2023-02-27 08:50:20 -0600

    Polynomial Regression examples

--- Python/Data analysis/FuelConsumption.py
+++ Python/Data analysis/FuelConsumption.py
@@ -46,7 +46,7 @@ plt.xlabel("Engine size")
 plt.ylabel("Emission")
 plt.show()
 
-#Modeling
+#Modeling Polynominal regression
 # Using sklearn package to model data.
 
 from sklearn import linear_model
@@ -66,11 +66,36 @@ plt.ylabel("Emission")
 
 
 from sklearn.metrics import r2_score
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
 
 test_x = np.asanyarray(test[['ENGINESIZE']])
 test_y = np.asanyarray(test[['CO2EMISSIONS']])
-test_y_ = regression.predict(test_x)
+
+
+poly = PolynomialFeatures(degree=2)
+train_x_poly = poly.fit_transform(train_x)
+train_x_poly
+
+clf = linear_model.LinearRegression()
+train_y_ = clf.fit(train_x_poly, train_y)
+# The coefficients
+print ('Coefficients: ', clf.coef_)
+print ('Intercept: ',clf.intercept_)
+
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='yellow')
+XX = np.arange(0.0, 10.0, 0.1)
+yy = clf.intercept_[0]+ clf.coef_[0][1]*XX+ clf.coef_[0][2]*np.power(XX, 2)
+plt.plot(XX, yy, '-r' )
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+from sklearn.metrics import r2_score
+
+test_x_poly = poly.transform(test_x)
+test_y_ = clf.predict(test_x_poly)
 
 print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
 print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
-print("R2-score: %.2f" % r2_score(test_y , test_y_) )
+print("R2-score: %.2f" % r2_score(test_y,test_y_ ) )
